079880818566206c26854a1cd0d985682681c9e1
p <ptrpfa@gmail.com>
2019-10-23 16:55:40 +0800

    5pm

--- test.py
+++ test.py
@@ -8,9 +8,9 @@ train_file_path = "/home/p/Desktop/csitml/train.csv"
 train_data = pd.read_csv (train_file_path, index_col="PassengerId")
 
 # 2) Understand dataset
-train_data.hist() # Univariate histogram
-train_data.plot(kind='density', subplots=True, layout=(3,3), sharex=False) # Univariate density plot
-train_data.plot(kind = 'box', subplots = True, layout = (3,3), sharex = False,sharey = False) # Univariate box plot
-scatter_matrix (train_data) # Multivariate scatter matrix
+# train_data.hist() # Univariate histogram
+# train_data.plot(kind='density', subplots=True, layout=(3,3), sharex=False) # Univariate density plot
+# train_data.plot(kind = 'box', subplots = True, layout = (3,3), sharex = False,sharey = False) # Univariate box plot
+# scatter_matrix (train_data) # Multivariate scatter matrix
 
-print (train_data.head ())
+print (train_data.head ()) # View dataset
