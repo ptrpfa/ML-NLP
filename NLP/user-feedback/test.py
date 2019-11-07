c68d45ca20725781f8e94758ba451df4c42f1940
p <ptrpfa@gmail.com>
2019-11-07 16:48:54 +0800

    meh

---
+++ NLP/user-feedback/test.py
@@ -0,0 +1,13 @@
+import pandas as pd
+import numpy as np
+import re # REGEX
+import pickle 
+from sklearn.model_selection import train_test_split # 4) For splitting dataset into train/test sets
+from sklearn import linear_model # 4) Linear Regression classifier
+from sklearn.naive_bayes import GaussianNB # 4) Naive Bayes classifier
+from sklearn.ensemble import RandomForestClassifier # 4) Random Forest classifier
+from sklearn.metrics import accuracy_score # 4) Accuracy scorer
+from sklearn.model_selection import cross_val_score # 4) Cross validation scorer
+from sklearn.model_selection import GridSearchCV # 4) For model hyperparameters tuning
+
+# Create spam-detection model to filter out spam
