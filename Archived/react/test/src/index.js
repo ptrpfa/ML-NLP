68c20cdccf9bc4c33884b89993996aacfdecbfef
p <ptrpfa@gmail.com>
2019-11-13 16:18:29 +0800

    spam detection model overfit

--- Archived/react/test/src/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-
-const myfirstelement = <h1>Hello React!</h1>
-test = <h5>alert('lol');</h5>
-
-ReactDOM.render(myfirstelement, document.getElementById('root'));
-ReactDOM.render(test, document.getElementById('lol'));
--- Archived/titanic.py
+++
@@ -1,6 +0,0 @@
-# Import libraries
-import pandas as pd
-
-titanic_train = pd.read_csv ("/home/p/Desktop/csitml/train.csv")
-
-print (titanic_train)
